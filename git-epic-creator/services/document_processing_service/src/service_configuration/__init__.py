c926a014e911b8cbe0856972b6fc3ff382e73809
Mr. Capybara <email@gmail.com>
2025-09-24 19:04:58 +0200

    add docling

--- git-epic-creator/services/document_processing_service/src/service_configuration/__init__.py
+++ git-epic-creator/services/document_processing_service/src/service_configuration/__init__.py
@@ -1,10 +1,6 @@
 """Service-specific configuration module for document processing service."""
 
 from .tika_config import TikaSettings
-
-try:
-    from .docling_config import DoclingSettings  # type: ignore
-except Exception:  # pragma: no cover - optional during migration
-    DoclingSettings = None  # type: ignore
+from .docling_config import DoclingSettings
 
 __all__ = ['TikaSettings', 'DoclingSettings']
